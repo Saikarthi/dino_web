3705b384c4c30d48936a41ef854bf457e3febedb
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2021-03-03 22:05:06 +0530

    start

---
+++ dino-web/Assets/code/move.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed=0.1f;
+
+
+
+    void Update()
+    {
+        this.transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
+    }
+}
